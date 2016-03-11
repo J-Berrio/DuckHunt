25d49fa8421fd6026c0d53347558bc82ebf5abf3
Julia Berrio <berrioja@gmail.com>
2016-03-11 09:23:12 -0700

    adding weapons mech, shooting etc

--- DuckHunt 2/Assets/Scripts/PlayerMove1.cs
+++ DuckHunt 2/Assets/Scripts/PlayerMove1.cs
@@ -4,11 +4,25 @@ using System.Collections;
 public class PlayerMove1 : MonoBehaviour 
 {
 	private Rigidbody rb;
+	private float nextFire;
+
 	public float speed;
+	public GameObject shot;
+	public Transform ShotsSpawn;
+	public float fireRate;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+			if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+				nextFire = Time.time + fireRate;
+				Instantiate(shot, ShotsSpawn.position, ShotsSpawn.rotation);
+		}
 	}
 
 	void FixedUpdate ()
