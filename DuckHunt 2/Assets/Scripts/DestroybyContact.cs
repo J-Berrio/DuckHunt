f37c4bfc938feda50c05e1c752e1beb1c9fde737
Julia Berrio <berrioja@gmail.com>
2016-03-30 09:06:33 -0600

    fixing player movement, destroy by boundary, rotator, etc. Should've made a branch.

---
+++ DuckHunt 2/Assets/Scripts/DestroybyContact.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroybyContact : MonoBehaviour 
+{
+	public GameObject explosion;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy(other.gameObject);
+		Destroy (gameObject);
+	}
+}
